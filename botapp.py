d21c391474e337087d834d2dd954d1bde8918148
Noritoshi ATSUMI <atsumi.noritoshi.5u@kyoto-u.ac.jp>
2021-08-02 17:35:35 +0900

    Update botapp.py

--- botapp.py
+++ botapp.py
@@ -3,7 +3,7 @@ from slackbot.bot import Bot
 import os
 
 slack = Slacker(os.environ['API_TOKEN'])
-slack.chat.post_message('#slackbot-app', 'new version bot is deployed.', as_user=True)
+slack.chat.post_message(os.environ['CHANNEL'], 'new version bot is deployed.', as_user=True)
 
 mybot = Bot()
 mybot.run()
